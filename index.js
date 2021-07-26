0d4df03886eb3d0ff55d7d0461cd988b11859c2d
Johnson Tavares <jh.tavares.jh@gmail.com>
2021-07-25 20:33:34 -0400

    Alterando dados

---
+++ index.js
@@ -0,0 +1,12 @@
+function clicou(){
+    window.location.href= "../Cliente-register/Cliente-register.html"
+}
+
+function cadastrado(){
+    alert("Dados cadastrados com sucesso !")
+    window.location.href= "Cliente-register.html"
+}
+
+function RegisterProduct(){
+    window.location.href = "../Product-register/index.html"
+}
